29bbb92087b53f8b85f71d9144af62830a794477
Nick Lewis <nick@puppetlabs.com>
2010-06-25 16:15:12 -0700

    maint: Working Brew provider! Need to hack Homebrew a bit to make it do some of the other things I need though.

--- lib/puppet/provider/package/brew.rb
+++ lib/puppet/provider/package/brew.rb
@@ -7,7 +7,7 @@ Puppet::Type.type(:package).provide :brew, :parent => Puppet::Provider::Package 
     commands :brewcmd => "brew"
 
     def install
-        command = [command(:brew), "install"]
+        command = [command(:brewcmd), "install"]
         command << resource[:name]
         output = execute(command)
         unless $? == 0
@@ -15,18 +15,41 @@ Puppet::Type.type(:package).provide :brew, :parent => Puppet::Provider::Package 
         end
     end
 
-    def latest
-        install(:version => "--HEAD")
-    end
-
-    def query
-
-    end
-
     def uninstall
+        command = [command(:brewcmd), "uninstall"]
+        command << resource[:name]
+        output = execute(command)
+        unless $? == 0
+            self.fail "Could not uninstall: %s" % output.chomp
+        end
     end
 
     def update
-        command = [command(:brew), "update", resource[:name]]
+        command = [command(:brewcmd), "update", resource[:name]]
+        output = execute(command)
+        unless $? == 0
+            self.fail "Could not update: %s" % output.chomp
+        end
+    end
+
+    def query
+        not self.class.cellar.find { |keg| keg[:name] == resource[:name] }.nil?
+    end
+
+    def self.instances
+        cellar.collect do |keg|
+            new(keg)
+        end
+    end
+
+    def self.cellar
+        command = [command(:brewcmd), "list"]
+        output = execute(command)
+        unless $? == 0
+            raise Puppet::Error.new "Could not list packages: %s" % output.chomp
+        end
+        output.split.collect do |keg|
+            { :name => keg, :ensure => :present, :provider => :brew }
+        end
     end
 end
