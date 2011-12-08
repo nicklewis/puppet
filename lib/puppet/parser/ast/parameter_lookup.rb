de488256e7615002752101117df14ef914ba1dca
Nick Lewis <nick@puppetlabs.com>
2011-12-08 15:13:15 -0800

    (#2611) Implement resource attribute access with . operator
    file { "/tmp/foo":
      content => 'something',
    }
    
    file { "/tmp/bar":
      content => File['/tmp/foo'].content,
    }
    
    This manifest will set content of both /tmp/foo and /tmp/bar to
    'something'.

---
+++ lib/puppet/parser/ast/parameter_lookup.rb
@@ -0,0 +1,25 @@
+require 'puppet/parser/ast'
+require 'puppet/parser/ast/branch'
+
+class Puppet::Parser::AST::ParameterLookup < Puppet::Parser::AST::Branch
+  attr_accessor :resource, :parameter
+
+  def evaluate(scope)
+    real_resources = [resource.evaluate(scope)].flatten
+
+    invalid_resources = real_resources.select {|r| !r.is_a? Puppet::Resource}
+    if invalid_resources.any?
+      raise Puppet::ParseError, "Parameter lookup must be on a Puppet::Resource, not #{invalid_resources.first.class}"
+    end
+
+    values = real_resources.map do |res|
+      scope.findresource(res.ref)[parameter.to_sym]
+    end
+
+    values.length == 1 ? values.first : values
+  end
+
+  def to_s
+    "#{resource.to_s}.#{parameter}"
+  end
+end
