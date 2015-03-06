f0d548e3e98d2b8e68a8ca90b2d8f20e2316a884
Nick Lewis <nick@puppetlabs.com>
2015-03-06 10:02:56 -0800

    Simplify environment catalog format
    Previously we were returning a structure like
    
    {"Forge[production]" =>
      {"Web[w1]" => {...},
       "Web[w2]" => {...}}
    
    This was cumbersome to work with because it used resource refs to refer
    to resources, and particularly because it used those resource refs as
    hash keys.
    
    Now we refer to resource using a combination of :type/:title, so we
    don't need to parse the refs later. This is also more consistent with
    the structure of agent catalogs. The structure has also been flattened,
    so that we represent an application (and each of its component) within a
    single map, rather than spread out between the key and value of a
    collection.
    
    [{:type => "Forge",
      :title => "production",
      :components => [{:type => "Web", :title => "w1", ...},
                      {:type => "Web", :title => "w2", ...}]}]
    
    This minimizes the amount of processing we need to do in order to
    actually consume this data structure.

--- lib/puppet/network/http/api/master/v3/environment.rb
+++ lib/puppet/network/http/api/master/v3/environment.rb
@@ -8,20 +8,17 @@ class Puppet::Network::HTTP::API::Master::V3::Environment
 
     catalog = Puppet::Parser::EnvironmentCompiler.compile(env).to_resource
 
-    env_graph = {:environment => env.name, :applications => {}}
     applications = catalog.resources.select do |res|
       type = res.resource_type
       type.is_a?(Puppet::Resource::Type) && type.application?
     end
-    applications.each do |app|
-      app_components = {}
-      catalog.direct_dependents_of(app).each do |comp|
+    app_instances = applications.map do |app|
+      app_components = catalog.direct_dependents_of(app).map do |comp|
         type = comp.resource_type
         params = comp.to_hash
         consumes = params.select { |p| Array(type.consumes).include?(p) }.values
-        consumes = consumes.flatten.map(&:ref)
-        params = params.select { |p| ! type.consumes.include?(p) }
-        produces = catalog.direct_dependents_of(comp)
+        consumes = consumes.flatten.map { |c| {:type => c.type, :title => c.title} }
+        produces = catalog.direct_dependents_of(comp).map { |p| {:type => p.type, :title => p.title} }
 
         mapped_nodes = app['nodes'].select { |node, components| components.map(&:ref).include?(comp.ref) }.map { |node, components| node.title }
 
@@ -33,10 +30,11 @@ class Puppet::Network::HTTP::API::Master::V3::Environment
           mapped_node = mapped_nodes.first
         end
 
-        app_components[comp.ref] = {:produces => produces.map(&:ref), :consumes => consumes, :node => mapped_node}
+        {:type => comp.type, :title => comp.title, :produces => produces, :consumes => consumes, :node => mapped_node}
       end
-      env_graph[:applications][app.ref] = app_components
+      {:type => app.type, :title => app.title, :components => app_components}
     end
+    env_graph = {:environment => env.name, :applications => app_instances}
     response.respond_with(200, "application/json", JSON.dump(env_graph))
   end
 end
